a6aabf7a4dd5740ece51078195773ea49f913759
Jesse B. Hannah <jesse@jbhannah.net>
2014-12-24 11:20:06 -0700

    Application view helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  include FoundationRailsHelper::FlashHelper
+
+  def controller_classes
+    controller_path.gsub '/', ' '
+  end
+
+  def join_title(*args)
+    args.join(" | ")
+  end
 end
