30f87de75392707511f411e5707e0cf15cb7096d
Jesse B. Hannah <jesse@jbhannah.net>
2014-12-24 10:30:07 -0700

    Reset test database before each suite

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,7 +20,15 @@ require 'rspec/rails'
 #
 # Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
+def delete_db
+  Neo4j::Session.current._query('MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r')
+end
+
 RSpec.configure do |config|
+  config.before(:suite) do
+    delete_db
+  end
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
